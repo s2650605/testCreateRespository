238c101f911144de54386e5640c1ab43b5bba5b7
shenxiang <250811095@qq.com>
2020-04-27 16:00:10 +0800

    新建

---
+++ test_git/src/main/java/com/laoshentou/TestGit.java
@@ -0,0 +1,11 @@
+package com.laoshentou;
+
+/**
+ * @author shenxiang
+ * @date 2020年04月27日, 0027 15:59
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+}
